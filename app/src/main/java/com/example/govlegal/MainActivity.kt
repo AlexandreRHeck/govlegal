6f4d231c3d548b283870146bcf4b62ef1f45b4e5
Alexandre Heck <35086774+AlexandreRHeck@users.noreply.github.com>
2023-01-06 18:40:54 -0300

    adicionado verificaçao Internet e goback das telas

--- app/src/main/java/com/example/govlegal/MainActivity.kt
+++ app/src/main/java/com/example/govlegal/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.govlegal
-
 import android.annotation.SuppressLint
 import android.app.AlertDialog.Builder
+import android.content.Context
+import android.net.ConnectivityManager
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.govlegal.databinding.ActivityMainBinding
 import com.onesignal.OneSignal;
@@ -12,53 +17,63 @@ import com.onesignal.OneSignal;
 //2d365516-416c-4669-861f-7bb048934409  // esse é govlegal produçao
 
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
+
     private final val ONESIGNAL_APP_ID: String = "2d365516-416c-4669-861f-7bb048934409"
-    private lateinit var  binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
 
-
-    @SuppressLint("SetJavaScriptEnabled")
+    @SuppressLint("SetJavaScriptEnabled", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val webView = binding.webView //findViewById<WebView>(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://app.govlegal.com.br/#/")
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+
 
         // Habilite o log detalhado do OneSignal para depurar problemas, se necessário.
         OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
-
         // OneSignal Initialization
         OneSignal.initWithContext(this);
         OneSignal.setAppId(ONESIGNAL_APP_ID);
         // promptForPushNotifications mostrará o prompt de permissão de notificação nativa do Android.
         OneSignal.promptForPushNotifications();
 
-
-
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val webWiew = binding.webView
-
-
-        webWiew.loadUrl("https://app.govlegal.com.br/#/")
-        webWiew.settings.javaScriptEnabled = true
-        webWiew.settings.domStorageEnabled = true
-
-
+        // Verifica conexao internet
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        if (networkInfo != null && networkInfo.isConnected) {
+            // Estamos conectados à Internet
+            Toast.makeText(this,"Conectado a Internet",Toast.LENGTH_SHORT).show()
+        } else {
+            // Não estamos conectados à Internet
+            Toast.makeText(this,"Não conectado a Internet",Toast.LENGTH_SHORT).show()
+            setContentView(R.layout.network_error_layout)
+        }
 
     }
 
-
     override fun onBackPressed() {
-        //super.onBackPressed()
-        autorisandoSaida();
+        val webView = binding.webView//findViewById<WebView>(R.id.webView)
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            autorisandoSaida();
+           // super.onBackPressed()
+        }
     }
 
     private fun autorisandoSaida(){
 
         val builder = Builder(this)
-
         builder.setIcon(R.mipmap.ic_launcher)
         builder.setTitle(R.string.app_name)
         builder.setMessage("Voce Deseja sair do GOVLEGAL?")
---
+++ app/src/main/java/com/example/govlegal/NetworkConnection.kt
@@ -0,0 +1,69 @@
+package com.example.govlegal
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.media.browse.MediaBrowser.ConnectionCallback
+import android.net.ConnectivityManager
+import android.net.ConnectivityManager.NetworkCallback
+import android.net.Network
+import android.net.NetworkInfo
+import android.os.Build
+import androidx.lifecycle.LiveData
+import com.google.android.datatransport.cct.internal.NetworkConnectionInfo
+
+@Suppress("DEPRECATION")
+class NetworkConnection (private val context: Context): LiveData<Boolean>(){
+
+    private val connectivityManager: ConnectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    private lateinit var networkConnectionCallback: NetworkCallback
+    override fun onActive() {
+        super.onActive()
+        updateNetworkConnection()
+        when{
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.N ->{
+                connectivityManager.registerDefaultNetworkCallback(connectionCallback())
+            }else -> {
+                context.registerReceiver(networkReceiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+            }
+        }
+    }
+
+
+
+    override fun onInactive() {
+        super.onInactive()
+        connectivityManager.unregisterNetworkCallback(connectionCallback())
+    }
+
+    private fun connectionCallback(): NetworkCallback{
+
+        networkConnectionCallback = object : NetworkCallback(){
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                postValue(true)
+            }
+
+            override fun onLost(network: Network) {
+                super.onLost(network)
+                postValue(false)
+            }
+        }
+        return networkConnectionCallback
+
+    }
+
+    private fun updateNetworkConnection() {
+
+        val networkConnection:NetworkInfo? = connectivityManager.activeNetworkInfo
+        postValue(networkConnection?.isConnected == true)
+    }
+    private val networkReceiver = object : BroadcastReceiver(){
+        override fun onReceive(p0: Context?, p1: Intent?) {
+            updateNetworkConnection()
+        }
+    }
+
+}
